7adcd80de36118c453913e7206a53f480eaa4a3d
ThoNguyen3579 <tknguyen@smith.edu>
2026-02-17 23:53:18 -0500

    A3 almost there!!

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,68 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 // You should **not** update any call signatures in this file
 // only modify the body of each function
+
 class Conversation implements ConversationRequirements {
 
   // Attributes 
-
+  ArrayList<String> transcript; // a modifiable list containing the conversation
+  String [] canned_responses = {"Okay", "That's interesting!", "Hmm..."}; //  an unmodifiable list with responses
   /**
    * Constructor 
    */
   Conversation() {
-    
+    // initialize the constructor
+    this.transcript = new ArrayList<String>(); 
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    Scanner input = new Scanner(System.in); // only one scanner needed
 
-  }
+    // Ask user to input the number of rounds they want to run 
+    System.out.println("Number of rounds:");
+    int rounds = input.nextInt();
+
+    // Print a statement, save that statement in transcript, and start the conversation
+    System.out.println("Hi there! What's on your mind?");
+    this.transcript.add("Hi there! What's on your mind?");
+    // get user response and turn them into a string
+    String userresponse1 = input.nextLine(); 
+
+
+    // Create a for loop with the number of rounds required for the dialogue between the chat and user,
+    // 1. take the user input
+    // 2. save that input into the transcript
+    // 3. call the method respond to generate the response and save this response in the transcript
+    for(int i=0; i< rounds; i++){
+        String userresponse = input.nextLine();
+        this.transcript.add(userresponse);
+        String response_to_user = respond(userresponse);
+        System.out.println(response_to_user);
+        this.transcript.add(response_to_user);
+        
+      } 
+    // print and save goodbye message
+    System.out.println("See ya!");
+    this.transcript.add("See ya!");
+    }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
+    // From the array list transcript, turn them into a string with old elements now separated by the new line character.
+    String new_transcript = String.join("\r\n", this.transcript);
+    // print the transcript of conversation
+    System.out.print(new_transcript);
 
   }
 
@@ -31,8 +72,46 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    // create the returnString
+    String returnString = "";
+    // create a list to prepare for the array by turning the characters into all lowercase, and then split them through space
+    String [] prep_arr = inputString.toLowerCase().split(" ");
+    // create an array list called arr that has the elements of prep_arr
+    ArrayList<String> arr = new ArrayList<>();
+    for (int i = 0; i < prep_arr.length; i++){
+      arr.add(prep_arr[i]);
+    }
+    // create a random object
+    Random rand = new Random();
+
+    // if the elements in arr contains the mirrored words
+    if (arr.contains("i") || arr.contains("me") || arr.contains("you")
+    || arr.contains("am") || arr.contains("my") || arr.contains("your")){
+      // run a loop to check each elements inside arr and replace them accordingly with the mirrored word
+      for (int i = 0; i < prep_arr.length; i++){
+        if ("i".equals(prep_arr[i]) || "me".equals(prep_arr[i])){
+          arr.replaceAll(s -> s.replace("i", "you"));
+          arr.replaceAll(s -> s.replace("me","you"));
+        } else if ("you".equals(prep_arr[i])){
+          arr.replaceAll(s -> s.replace("you","I"));
+        } else if ("am".equals(prep_arr[i])){
+          arr.replaceAll(s -> s.replace("am","are"));
+        } else if ("my".equals(prep_arr[i])){
+          arr.replaceAll(s -> s.replace("my","your"));
+        } else if ("your".equals(prep_arr[i])){
+          arr.replaceAll(s -> s.replace("your","my"));
+        }
+      returnString = String.join(" ", arr);
+      returnString = returnString + "?";
+      }
+    }
+    // if there is no mirrored word inside arr
+    else {
+      // the returnString would be a randomized canned response
+        returnString = this.canned_responses[rand.nextInt(this.canned_responses.length)];
+      }
+    
+    return returnString;
   }
 
   public static void main(String[] arguments) {
--- reflection.md
+++ reflection.md
@@ -1,7 +1,17 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
-- Which session(s) of TA / office hours did you attend?
+- Which classmates did you consult with while working on this assignment? 
+Emmarie Miller! We are collaborating and she is amazingly helpful!
+- Which session(s) of TA / office hours did you attend? 
+I attended the TA hours on Monday.
+
 - What are your initial impressions of Java? 
+Honestly, I have only known about Java in this class, and it is pretty scary to learn such a different language.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+I jut feel like Java, in addition to the Object Oriented Programming, is so much the same as learning a new language with a little bit resemblance to Python. It feels more complicated and requires more details (classes, contributors, etc.). Java also requires very careful type definition.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+It just took me lots of time to figure things out, so I am sooo grateful to have my collaborator and the TA help me! They helped me with the for loop for the conversation and the randomization part, and I just fully understood it thanks to them! 
+Personally, I really appreciate the brainstorming part (although all my brain cells are exploding when doing A3!) because it allows me to develop the logic and syntax for Java so well! This task also proves the importance of collaboration, because I know that I would not have been able to finish this task by myself alone!
+Although I am done with most of the assignment, the mirroring part is still a bit funky (the space, the punctuation). Hia has suggested me instead of changing the inputString, we create returnString as a copy the inputString and then replace the words in the returnString. However, that have not really solved the spacing problem (since we still have to split the string into words in order to make it really work). And also, if my mirror word has punctuation (for instance: thank you.), it will answer "thank you.?" instead of "thank I?". However, I cannot use replace because the replace function will change the letter instead of the whole word (for instance, thinking -> thyounkuoung). Also, currently, I have not figured out how to rule out certain words that contain the mirror words like "I" (for instance: thinking).
